57321a112d4c7840dde97b97763a0451a7011eec
Farrel Amroe Azhari <88188954+farrelamroe@users.noreply.github.com>
2022-08-21 21:34:18 +0700

    Update opening_video.jsx

--- src/components/home/opening_video.jsx
+++ src/components/home/opening_video.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Penari from '../../assets/home/penari.png'
+import logo from '../../assets/logo_fgm.svg'
+import festival_footer from '../../assets/footer/festival-gm-footer.svg';
 
 
 const opening_video = () => {
   return (
-    <div>
-      <img className='w-full relative ' src={Penari}>
-      </img>
+    <div className="w-full relative flex justify-center px-[10px] py-[142px]" style={{backgroundImage: `url(${Penari})`}}>
+      <div className=" mr-[500px] flex justify-center flex-row">
+        <img  className="px-[10px]" src={logo} alt="logo"></img>
+        <img  className="" src={festival_footer} alt="text"></img>
+      </div>
     </div>
   )
 }
