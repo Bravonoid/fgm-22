d57d95983c497f7cc6445f3c9bb90831ba7926f5
Farrel Amroe Azhari <88188954+farrelamroe@users.noreply.github.com>
2022-08-21 22:30:06 +0700

    Update opening_video.jsx

--- src/components/home/opening_video.jsx
+++ src/components/home/opening_video.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Penari from '../../assets/home/penari.png'
-import logo from '../../assets/logo_fgm.svg'
-import festival_footer from '../../assets/footer/festival-gm-footer.svg';
+import Penari from '../../assets/home/penari.png';
+import fgm_black from '../../assets/home/fgm-black.svg';
+import mozaik from '../../assets/home/mozaik.svg'
 
 
 const opening_video = () => {
   return (
-    <div className="w-full relative flex justify-center px-[10px] py-[142px]" style={{backgroundImage: `url(${Penari})`}}>
+    <div className="w-full relative flex justify-center px-[10px] py-[1px]" style={{backgroundImage: `url(${Penari})`}}>
       <div className=" mr-[500px] flex justify-center flex-row">
-        <img  className="px-[10px]" src={logo} alt="logo"></img>
-        <img  className="" src={festival_footer} alt="text"></img>
+        <img className="mr-[150px] p-[10px]" src={mozaik} alt="mozaik"></img>
+        <img  className="px-[10px]" src={fgm_black} alt="logo"></img>
       </div>
     </div>
   )
