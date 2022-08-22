66eae9864ded86efe8b7ee9ba7082348e70a5460
hervibest <71449881+hervibest@users.noreply.github.com>
2022-08-22 14:13:35 +0700

    benerin logo rangkaian acara + script buat linimasa

--- src/components/home/rangkaian.jsx
+++ src/components/home/rangkaian.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Cards from '../home/tentang/index'
+import Pattern from '../../assets/home/rangakaian/pattern-rangkaian.svg'
+import Rangkaian from '../../assets/home/rangakaian/rangkaian-acara.svg'
+
+
+
 const rangkaian = () => {
   return (
-    <div className='w-full flex  h-[75vh] justify-center items-center  bg-[#171717]'>
+    <div className='w-full flex z-10 flex-col  h-[75vh] justify-center items-center  bg-[#171717]'>
+      <img className='relative  ' src={Rangkaian} title="judul" ></img>
       <div className=' relative my-5 -top-10 mx-auto justify-center w-3/5 items-center text-left text-white'>
       <div className="flex flex-col md:flex-row">
             <Cards />
           </div>
 
       </div>
+      <img className='relative  ' src={Pattern} title="yes" >
+
+</img>
     </div>
   )
 }
