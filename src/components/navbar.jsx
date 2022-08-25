d9bfc84104d004f2697b95b34de78edc545d59f4
Farrel Amroe Azhari <88188954+farrelamroe@users.noreply.github.com>
2022-08-25 23:48:37 +0700

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../assets/home/logo-navbar.svg';
 
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
 
-const navbar = () => {
   return (
-      <div className='w-full h-16 bg-black relative top-0 items-center px-10 text-white justify-end flex'>
-        <div className="flex items-center flex-shrink-0 px-2 mx-[300px]">
+      <div className='w-full h-16 bg-black relative flex text-white justify-between'>
+        <div className="flex items-center flex-shrink-0 px-[10px] py-[10px] sm:mx-0">
           <div className="h-full w-full">
               <img src={Logo} alt="Logo" className="hover:cursor-pointer" />
           </div>
         </div>
-        <ul className='flex justify-between mx-10 gap-20 text-xl hover:cursor-pointer'>
-            <li className="hover:bg-red-500 rounded-full p-2">Beranda</li>
-            <li className="hover:bg-red-500 rounded-full p-2">Tentang</li>
-            <li className="hover:bg-red-500 rounded-full p-2">Event</li>
-            <li className="hover:bg-red-500 rounded-full p-2">Ticket</li>
-
-            </ul>
+        <ul className='flex items-center justify-between mx-10 gap-20 text-xl hover:cursor-pointer sm:hidden lg:flex lg:ml-auto py-2'>
+            <li className="hover:bg-pattern hover:bg-cover p-2">Beranda</li>
+            <li className="hover:bg-pattern hover:bg-cover p-2">Tentang</li>
+            <li className="hover:bg-pattern hover:bg-cover p-2">Event</li>
+            <li className="hover:bg-pattern hover:bg-cover p-2">Ticket</li>
+        </ul>
+        <div className="lg:hidden items-center">
+						<button onClick={() => setNavbar(!navbar)} className="outline-none mobile-menu-button items-center my-[20px]">
+						<svg className=" w-6 h-6 text-gray-500 hover:text-green-500"
+							x-show="!showMenu"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button>
+				</div>
+        <div className={`${navbar?"inline-block":"hidden"} mobile-menu`}>
+          <ul className="flex flex-col bg-green-300">
+            <li className="active"><a href=" " className="block text-sm px-2 py-4 text-white font-semibold">Beranda</a></li>
+            <li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Tentang</a></li>
+            <li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Event</a></li>
+            <li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Ticket</a></li>
+          </ul>
+			  </div>
       </div>
   )
 }
 
-export default navbar;
+export default Navbar;
