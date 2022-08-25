e5e70403b5155bf3a724dd9e79fd259d7b9ee1d7
Farrel Amroe Azhari <88188954+farrelamroe@users.noreply.github.com>
2022-08-25 23:47:26 +0700

    Update tentang.jsx

--- src/components/home/tentang.jsx
+++ src/components/home/tentang.jsx
@@ -1,47 +1,43 @@
-import React, {useState} from 'react';
-import Logo from '../assets/home/logo-navbar.svg';
+import React from 'react'
+import Logo from '../../assets/logo_fgm.svg';
+import gunungan from '../../assets/home/gunungan.svg';
+import mozaik_horizontal from '../../assets/home/mozaik_horizontal.svg';
+import rectangle_17 from '../../assets/home/rectangle-17.svg';
+import rectangle_18 from '../../assets/home/rectangle-18.svg';
 
-const Navbar = () => {
-  const [navbar, setNavbar] = useState(false);
 
+
+const tentang = () => {
   return (
-      <div className='w-full h-16 bg-black relative flex text-white justify-between'>
-        <div className="flex items-center flex-shrink-0 px-[10px] py-[10px] sm:mx-0">
-          <div className="h-full w-full">
-              <img src={Logo} alt="Logo" className="hover:cursor-pointer" />
+    <div className='w-full h-[75vh] justify-end items-center  bg-[#1E1E1E]'>
+      <img src={gunungan} className='absolute sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6' alt='gunungan'></img>
+      <div className='grid relative sm:top-32 sm:-right-8 xl:-right-40 xl:top-48 xl:top-48 xl:ml-[50px] mx-auto grid-cols-2'>
+      <div className='relative my-5 -top-10  w-full items-end text-left text-white sm:mb-[100px] xl:-top-16'>
+        <div className='xl:text-5xl xl:mb-5 font-bold sm:text-3xl md:text-4xl lg:text-5xl'>
+        <img className='w-full' src={mozaik_horizontal} alt="mozaik"></img>
+          <div className='flex sm:gap-1.5 md:gap-1'>
+            <p>FESTIVAL </p>
+            <img className='sm:w-48 md:w-56 lg:w-72' src={rectangle_17} alt="rectangle"></img>
           </div>
-        </div>
-        <ul className='flex items-center justify-between mx-10 gap-20 text-xl hover:cursor-pointer sm:hidden lg:flex lg:ml-auto py-2'>
-            <li className="hover:bg-pattern hover:bg-cover p-2">Beranda</li>
-            <li className="hover:bg-pattern hover:bg-cover p-2">Tentang</li>
-            <li className="hover:bg-pattern hover:bg-cover p-2">Event</li>
-            <li className="hover:bg-pattern hover:bg-cover p-2">Ticket</li>
-        </ul>
-        <div className="lg:hidden items-center">
-						<button onClick={() => setNavbar(!navbar)} className="outline-none mobile-menu-button items-center my-[20px]">
-						<svg className=" w-6 h-6 text-gray-500 hover:text-green-500"
-							x-show="!showMenu"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path d="M4 6h16M4 12h16M4 18h16"></path>
-						</svg>
-					</button>
-				</div>
-        <div className={`${navbar?"inline-block":"hidden"} mobile-menu`}>
-          <ul className="flex flex-col bg-green-300">
-            <li className="active"><a href=" " className="block text-sm px-2 py-4 text-white font-semibold">Beranda</a></li>
-            <li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Tentang</a></li>
-            <li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Event</a></li>
-            <li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Ticket</a></li>
-          </ul>
-			  </div>
+          <div className='flex sm:gap-1'>
+            <p>GADJAH MADA</p>
+            <img className='sm:w-[105px] md:w-32 lg:w-40' src={rectangle_18} alt="rectangle"></img>
+          </div>
+          </div>
+        <div className='xl:text-xl xl:w-full sm:text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Donec tellus nulla, porta vel consectetur et,
+         mattis quis orci. In hac habitasse platea dictumst. Sed eget tincidunt risus. Mauris nec luctus sapien, eu cursus diam.</div>
+         <button className='border rounded-md px-[10px] mt-2 drop-shadow-2xl text-black bg-slate-300' >Selengkapnya</button>
+
+      </div>  
+      <img className=' sm:w-1/2 sm:top-1 sm:left-20 md:-top-0.5 lg:left-40 lg:w-2/5 relative xl:mb-1 xl:-top-20 justify-center xl:w-1/3 items-center text-left text-white' src={Logo} alt="logo">
+
+
+
+      </img>  
       </div>
+    </div>
   )
 }
 
-export default Navbar;
+export default tentang
