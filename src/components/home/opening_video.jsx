661c708a279bf651142a9b812a619f1dfa036a15
Farrel Amroe Azhari <88188954+farrelamroe@users.noreply.github.com>
2022-08-25 23:48:16 +0700

    Update opening_video.jsx

--- src/components/home/opening_video.jsx
+++ src/components/home/opening_video.jsx
@@ -6,10 +6,10 @@ import mozaik from '../../assets/home/mozaik.svg'
 
 const opening_video = () => {
   return (
-    <div className="w-full relative flex justify-center px-[10px] py-[1px]" style={{backgroundImage: `url(${Penari})`}}>
-      <div className=" mr-[500px] flex justify-center flex-row">
-        <img className="mr-[150px] p-[10px]" src={mozaik} alt="mozaik"></img>
-        <img  className="px-[10px]" src={fgm_black} alt="logo"></img>
+    <div className="w-full sm:object-left relative flex justify-center px-[10px] py-[1px]" style={{backgroundImage: `url(${Penari})`}}>
+      <div className='flex h-[450px]'>
+        <img className="sm:w-1/8 px-[10px] py-[10px] mx-[0px] lg:mr-[50px] lg:w-1/12 xl:mr-[100px]" src={mozaik} alt="fgm_black"></img>
+        <img className=' ml-[100px] pr-[10px] sm:w-2/3 lg:ml-[10px] xl:w-1/2' src={fgm_black} alt="fgm_black"></img>
       </div>
     </div>
   )
