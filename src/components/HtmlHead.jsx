8ebd8314a4e702cecd77c582979a3b65b04a3c89
farrelamroe@gmail.com <88188954+farrelamroe2003@users.noreply.github.com>
2022-10-15 07:51:06 +0700

    navvar

--- src/components/HtmlHead.jsx
+++ src/components/HtmlHead.jsx
@@ -1,10 +1,10 @@
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 
-// export function HtmlHead({ title, description }) {
-//   return (
-//     <Helmet>
-//       <title>{`${title} · Festival Gadjah Mada`}</title>
-//       <meta name="description" content={description} />
-//     </Helmet>
-//   );
-// }
+export function HtmlHead({ title, description }) {
+  return (
+    <Helmet>
+      <title>{`${title} · Festival Gadjah Mada`}</title>
+      <meta name="description" content={description} />
+    </Helmet>
+  );
+}
