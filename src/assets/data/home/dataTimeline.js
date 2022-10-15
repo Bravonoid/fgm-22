2ce25fca8b1b4aa6b5b40fdda34e0dfc93726844
Hervi Nur Rahmandien <71449881+hervibest@users.noreply.github.com>
2022-10-15 13:56:38 +0700

    Fix timeline content + infinite loop, button hover in tiket, cursor in teater

--- src/assets/data/home/dataTimeline.js
+++ src/assets/data/home/dataTimeline.js
@@ -1,48 +1,7 @@
 export const DataTanggalEvent =[
+    
     {
         id : 1,
-        title: "Early Bird Pentas Teater ",
-        lokasi: "KiosTix",
-        tanggal :"31 Agustus 2022",
-        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    },
-    {
-        id : 2,
-        title: "Early Bird Festival Seni",
-        lokasi: "KiosTix",
-        tanggal :"5 September 2022",
-        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    },
-    {
-        id : 3,
-        title: "Regular Pentas Teater",
-        lokasi: "KiosTix",
-        tanggal :"22 September 2022",
-        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    },
-    {
-        id : 4,
-        title: "Presale 1 Festival Seni ",
-        lokasi: "KiosTix",
-        tanggal :"28 September 2022",
-        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    },
-    {
-        id : 6,
-        title: "Presale 2 Festival Seni",
-        lokasi: "KiosTix",
-        tanggal :"22 Oktober 2022",
-        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    },
-    {
-        id : 7,
-        title: "Regular Festival Seni",
-        lokasi: "KiosTix",
-        tanggal :"6 November 2022",
-        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    },
-    {
-        id : 8,
         title: "Pentas Teater",
         lokasi: "Taman Budaya Yogyakarta",
         tanggal :"18 November 2022",
@@ -50,17 +9,24 @@ export const DataTanggalEvent =[
     }
     ,
     {
-        id : 9,
+        id : 2,
         title: "Pasar Rakyat",
         lokasi: "Lapangan Pancasila UGM",
         tanggal :"26 November 2022",
         linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
     },
     {
-        id : 10,
+        id : 3,
         title: "Festival Seni",
         lokasi: "Lapangan Pancasila UGM",
         tanggal :"26 November 2022",
         linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
-    }
+    }, {
+        id : 4,
+        title: "Regular Festival Seni",
+        lokasi: "KiosTix",
+        tanggal :"Segera Hadir",
+        linkImg:"https://firebasestorage.googleapis.com/v0/b/nesco-2022.appspot.com/o/Abstract-Design-Wallpaper-49_1644816088952_1646574379232_1651147566626.jpg?alt=media&token=3de621f9-818c-4014-872c-9d5f7bf0b12c"
+    },
+   
 ]
