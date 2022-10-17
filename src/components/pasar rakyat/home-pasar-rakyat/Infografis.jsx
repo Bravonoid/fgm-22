72a5f065f34f22cee9614bc99f6cff23ffca1d1e
clstyn <fiorenzacelestyn@gmail.com>
2022-10-17 20:45:41 +0700

    background pasar

--- src/components/pasar rakyat/home-pasar-rakyat/Infografis.jsx
+++ src/components/pasar rakyat/home-pasar-rakyat/Infografis.jsx
@@ -2,6 +2,7 @@ import TeksInfografisMobile from "../../../assets/Pasar-Rakyat/home-pasar/teks-i
 import KontenInfografisMobile from "../../../assets/Pasar-Rakyat/home-pasar/konten-infografis-mobile.svg";
 import TeksInfografis from "../../../assets/Pasar-Rakyat/home-pasar/teks-infografis.svg";
 import KontenInfografis from "../../../assets/Pasar-Rakyat/home-pasar/konten-infografis.svg";
+import BGPattern from "../../../assets/Pasar-Rakyat/home-pasar/bg-pasar.svg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -12,15 +13,36 @@ const Infografis = () => {
 
   return (
     <>
-      <div className="bg-[#202020] py-5 pb-12">
-        <img data-aos="zoom-in" data-aos-duration="1500" data-aos-offset='400' src={TeksInfografis} className="mx-auto hidden md:block" alt="" />
-        <img data-aos="zoom-in" data-aos-duration="1500" data-aos-offset='500'
+      <div
+        className="bg-[#202020] bg-contain bg-repeat py-5 pb-12"
+        style={{ backgroundImage: `url(${BGPattern})` }}
+      >
+        <img
+          data-aos="zoom-in"
+          data-aos-duration="1500"
+          data-aos-offset="400"
+          src={TeksInfografis}
+          className="mx-auto hidden md:block"
+          alt=""
+        />
+        <img
+          data-aos="zoom-in"
+          data-aos-duration="1500"
+          data-aos-offset="500"
           src={KontenInfografis}
           className="mx-auto hidden py-5 md:block"
           alt=""
         />
-        <img data-aos="fade-up" data-aos-duration="1500" src={TeksInfografisMobile} className="mx-auto md:hidden" alt="" />
-        <img data-aos="fade-up" data-aos-duration="1500"
+        <img
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          src={TeksInfografisMobile}
+          className="mx-auto md:hidden"
+          alt=""
+        />
+        <img
+          data-aos="fade-up"
+          data-aos-duration="1500"
           src={KontenInfografisMobile}
           className="mx-auto pt-10 md:hidden"
           alt=""
--- src/components/pasar rakyat/home-pasar-rakyat/SkemaPasar.jsx
+++ src/components/pasar rakyat/home-pasar-rakyat/SkemaPasar.jsx
@@ -1,6 +1,7 @@
 import TeksSkemaPasar from "../../../assets/Pasar-Rakyat/home-pasar/teks-skema-pasar-mobile.svg";
 import TeksSkema from "../../../assets/Pasar-Rakyat/home-pasar/teks-skema-pasar.svg";
 import Coming from "../../../components/coming_soon.jsx";
+import BGPattern from "../../../assets/Pasar-Rakyat/home-pasar/bg-pasar.svg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -10,10 +11,25 @@ const SkemaPasar = () => {
   AOS.refreshHard();
   return (
     <>
-      <div className="bg-[#202020] py-10">
-        <img data-aos="fade-down" data-aos-duration="1500" data-aos-offset="800" src={TeksSkema} className="mx-auto hidden md:block" alt="" />
+      <div
+        className="bg-[#202020] bg-contain bg-repeat py-10"
+        style={{ backgroundImage: `url(${BGPattern})` }}
+      >
+        <img
+          data-aos="fade-down"
+          data-aos-duration="1500"
+          data-aos-offset="800"
+          src={TeksSkema}
+          className="mx-auto hidden md:block"
+          alt=""
+        />
         <img src={TeksSkemaPasar} className="mx-auto md:hidden" alt="" />
-        <div data-aos="fade-down" data-aos-duration="1500" data-aos-offset="1000" className="py-10">
+        <div
+          data-aos="fade-down"
+          data-aos-duration="1500"
+          // data-aos-offset="1000"
+          className="py-10"
+        >
           <Coming />
         </div>
       </div>
