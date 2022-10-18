dfc21eeabae71f91dc6b6ab09df8f52c0fa1cb6b
clstyn <fiorenzacelestyn@gmail.com>
2022-10-18 15:23:39 +0700

    slider pasar rkayat

--- src/components/pasar rakyat/home-pasar-rakyat/TentangPasarRakyat.jsx
+++ src/components/pasar rakyat/home-pasar-rakyat/TentangPasarRakyat.jsx
@@ -3,6 +3,8 @@ import KembangTentangMobile from "../../../assets/Pasar-Rakyat/home-pasar/kemban
 import JudulTentangMobile from "../../../assets/Pasar-Rakyat/home-pasar/judul-tentang-mobile.svg";
 import Gambar1Tentang from "../../../assets/Pasar-Rakyat/home-pasar/gambar1-tentang.png";
 import Gambar2Tentang from "../../../assets/Pasar-Rakyat/home-pasar/gambar2-tentang.png";
+import Gambar3Tentang from "../../../assets/Pasar-Rakyat/home-pasar/gambar3-tentang.png";
+import Gambar4Tentang from "../../../assets/Pasar-Rakyat/home-pasar/gambar4-tentang.png";
 import KembangTentang from "../../../assets/Pasar-Rakyat/home-pasar/kembang-tentang.svg";
 import JudulTentang from "../../../assets/Pasar-Rakyat/home-pasar/judul-tentang.svg";
 import AOS from "aos";
@@ -12,21 +14,30 @@ const TentangPasarRakyat = () => {
   const settings = {
     arrows: false,
     dots: false,
-    infinite: true,
-    speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 0.5,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    infinite: true,
+    speed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
 
   AOS.init();
   AOS.refresh();
   AOS.refreshHard();
 
+  const styleDivSlider = "px-4 flex items-center justify-center";
+  const styleGambarSlider = "w-full lg:w-[500px] 2xl:mx-auto";
   return (
     <>
       <div className="items-center bg-[#171717] pb-[10rem] xl:pb-[20rem]">
-        <div className="relative md:top-24 md:mx-auto md:grid md:grid-cols-4 xl:top-48 mobile:top-0 mobile:flex mobile:flex-col" >
-          <div className="col-span-2 mt-8 hidden items-center justify-center md:block lg:mt-12 mobile:mt-32" data-aos="fade-right" data-aos-duration="1500">
+        <div className="relative md:top-24 md:mx-auto md:grid md:grid-cols-4 xl:top-48 mobile:top-0 mobile:flex mobile:flex-col">
+          <div
+            className="col-span-2 mt-8 hidden items-center justify-center md:block lg:mt-12 mobile:mt-32"
+            data-aos="fade-right"
+            data-aos-duration="1500"
+          >
             <div>
               <img src={KembangTentang} alt="" />
               <div className="bg-[#202020] py-5 px-10 xl:max-w-[596px]">
@@ -43,7 +54,11 @@ const TentangPasarRakyat = () => {
               kekinian
             </p>
           </div>
-          <div className="items-left relative col-span-2 mx-10 mr-24 pt-36 text-left text-white md:my-5 xl:w-3/4 mobile:mx-10 mobile:text-base" data-aos="fade-down" data-aos-duration="1500">
+          <div
+            className="items-left relative col-span-2 mx-10 mr-24 pt-24 text-left text-white md:my-5 md:pt-12 xl:w-3/4 mobile:mx-10 mobile:text-base"
+            data-aos="fade-down"
+            data-aos-duration="1500"
+          >
             <img src={KembangTentangMobile} className="md:hidden" alt="" />
             <img src={JudulTentangMobile} className="py-2 md:hidden" alt="" />
             <p className="font-bold md:hidden">26 November 2022 |</p>
@@ -55,11 +70,33 @@ const TentangPasarRakyat = () => {
               kekinian
             </p>
             <Slider className="pt-10" {...settings}>
-              <div className="mr-[-3rem]">
-                <img src={Gambar1Tentang} alt="" />
+              <div className={styleDivSlider}>
+                <img
+                  src={Gambar1Tentang}
+                  alt=""
+                  className={styleGambarSlider}
+                />
               </div>
-              <div className="ml-[-3rem]">
-                <img src={Gambar2Tentang} alt="" />
+              <div className={styleDivSlider}>
+                <img
+                  src={Gambar2Tentang}
+                  alt=""
+                  className={styleGambarSlider}
+                />
+              </div>
+              <div className={styleDivSlider}>
+                <img
+                  src={Gambar3Tentang}
+                  alt=""
+                  className={styleGambarSlider}
+                />
+              </div>
+              <div className={styleDivSlider}>
+                <img
+                  src={Gambar4Tentang}
+                  alt=""
+                  className={styleGambarSlider}
+                />
               </div>
             </Slider>
           </div>
