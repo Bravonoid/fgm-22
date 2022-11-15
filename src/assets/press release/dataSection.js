79ae0587050f35d9459bfeac91bbe5f227ce5e4b
farrelamroe@gmail.com <88188954+farrelamroe2003@users.noreply.github.com>
2022-11-15 20:32:59 +0700

    press_release

---
+++ src/assets/press release/dataSection.js
@@ -0,0 +1,12 @@
+const dataFoto = [
+    {
+        "name": "Foto 1",
+        "img": "IMG_0608",
+    },
+    {
+        "name": "Foto 2",
+        "img": "IMG_0603",
+    }
+]
+
+export {dataFoto};
