79aa3020d999d84cd1a1168eb0bc8ac0eac93bd9
HerviBest <hervipro@gmail.com>
2022-11-18 01:03:01 +0700

    goes to ending (teater utube)

--- src/components/teater/trailer.js
+++ src/components/teater/trailer.js
@@ -6,8 +6,8 @@ import Pattern from "../../assets/coming_soon/pattern.svg";
 const trailer = () => {
   return (
     <div className="inline-block h-full w-full bg-black  ">
-      <div className="mx-auto pointer-events-none my-auto w-full items-center px-12 py-48 sm:px-24 2xl:p-48 align-center justify-between">
-        <Coming_soon />
+      <div className="mx-auto my-auto w-full items-center 2xl:pt-12 pt-24 sm:px-2 px-6 lg:px-12 align-center justify-between">
+      <iframe className="2xl:w-[85%] 2xl:h-[85%] w-full h-full aspect-video relative mx-auto" src="https://www.youtube.com/embed/XLwFSC-zRVE" title="Iron to Ascendant - VALORANT INTERACTIVE STREAM #2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   );
